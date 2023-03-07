0e0482ee8dc5fda9aaeefdd27036f48446447909
bitdern <acnewell09@gmail.com>
2023-03-07 14:34:41 -0600

    learning about strings, ints, floats and bits

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+	// var nameOne string = "dernito"
+	// var nameTwo = "beanita"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi"
+
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 69
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits & memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint = 25
+
+	var scoreOne float32 = -1.6658
+	var scoreTwo float64 = 6666623881983.6699
+
+	fmt.Println(scoreOne, scoreTwo)
+}
